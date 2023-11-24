4c017e70b633e0753cd3a06fc37b0ee08f373e3b
Daniel Rovetta <daniel.rovettapassos@gmail.com>
2023-11-24 18:35:42 -0300

    iniciando pathfinder

--- pathfinder.py
+++ pathfinder.py
@@ -2,19 +2,32 @@ import tkinter as tk
 from tkinter import *
 import random
 import copy
+import math
 
 
 class Bloco:
-    j: int
     i: int
+    j: int
     estado: str
     valor: int
+    dh: str
+    dv: str
 
-    def __init__(self, j, i, valor, estado):
-        self.j = j
+    def __init__(self, i, j, valor, estado):
         self.i = i
+        self.j = j
         self.valor = valor
         self.estado = estado
+
+        if self.j == 0 or self.j % 2 == 0:
+            self.dh = "b"
+        else:
+            self.dh = "c"
+
+        if self.i == 0 or self.i % 2 == 0:
+            self.dv = "e"
+        else:
+            self.dv = "d"
 
 
 class Campo:
@@ -28,103 +41,111 @@ class Campo:
     def criarCampo(self):
         self.blocos = [[None] * self.colunas for _ in range(self.linhas)]
 
-        for j in range(self.linhas):
-            for i in range(self.colunas):
-                bloco = Bloco(j, i, 0, "vazio")
-                self.blocos[j][i] = bloco
+        for i in range(self.linhas):
+            for j in range(self.colunas):
+                bloco = Bloco(i, j, 0, "vazio")
+                self.blocos[i][j] = bloco
 
     def gerarObstaculos(self):
-        j = random.randint(0, self.linhas - 1)
-        i = random.randint(0, self.colunas - 1)
+        i = random.randint(0, self.linhas - 1)
+        j = random.randint(0, self.colunas - 1)
 
-        while self.blocos[j][i].estado != "vazio":
-            j = random.randint(0, self.linhas - 1)
-            i = random.randint(0, self.colunas - 1)
+        while self.blocos[i][j].estado != "vazio":
+            i = random.randint(0, self.linhas - 1)
+            j = random.randint(0, self.colunas - 1)
 
-        self.blocos[j][i].valor = 0
-        self.blocos[j][i].estado = "parede"
+        self.blocos[i][j].valor = 0
+        self.blocos[i][j].estado = "parede"
 
     def gerarInicio(self):
-        j = random.randint(0, self.linhas - 1)
-        i = random.randint(0, self.colunas - 1)
+        i = random.randint(0, self.linhas - 1)
+        j = random.randint(0, self.colunas - 1)
 
-        while self.blocos[j][i].estado != "vazio":
-            j = random.randint(0, self.linhas - 1)
-            i = random.randint(0, self.colunas - 1)
+        while self.blocos[i][j].estado != "vazio":
+            i = random.randint(0, self.linhas - 1)
+            j = random.randint(0, self.colunas - 1)
 
-        self.blocos[j][i].valor = 0
-        self.blocos[j][i].estado = "inicio"
-        self.blocos[j][i].j = j
-        self.blocos[j][i].i = i
+        self.blocos[i][j].valor = 0
+        self.blocos[i][j].estado = "inicio"
+        self.blocos[i][j].i = i
+        self.blocos[i][j].j = j
 
-        self.inicio = self.blocos[j][i]
+        self.inicio = self.blocos[i][j]
 
     def gerarFim(self):
-        j = random.randint(0, self.linhas - 1)
-        i = random.randint(0, self.colunas - 1)
+        i = random.randint(0, self.linhas - 1)
+        j = random.randint(0, self.colunas - 1)
 
-        while self.blocos[j][i].estado != "vazio":
-            j = random.randint(0, self.linhas - 1)
-            i = random.randint(0, self.colunas - 1)
+        while self.blocos[i][j].estado != "vazio":
+            i = random.randint(0, self.linhas - 1)
+            j = random.randint(0, self.colunas - 1)
 
-        self.blocos[j][i].valor = 0
-        self.blocos[j][i].estado = "fim"
-        self.blocos[j][i].j = j
-        self.blocos[j][i].i = i
+        self.blocos[i][j].valor = 0
+        self.blocos[i][j].estado = "fim"
+        self.blocos[i][j].i = i
+        self.blocos[i][j].j = j
 
-        self.fim = self.blocos[j][i]
+        self.fim = self.blocos[i][j]
 
     def criarInterface(self):
         campo = [[None] * self.colunas for _ in range(self.linhas)]
-        for j in range(self.linhas):
-            for i in range(self.colunas):
+        for i in range(self.linhas):
+            for j in range(self.colunas):
                 bloco = tk.Label(
                     self.root,
-                    width=5,
-                    height=2,
-                    text=self.blocos[j][i].valor,
+                    width=7,
+                    height=3,
+                    text=str(self.blocos[i][j].valor)
+                    + "\n"
+                    + self.blocos[i][j].dh
+                    + ":"
+                    + self.blocos[i][j].dv
+                    + "\n"
+                    + str(self.blocos[i][j].i)
+                    + ":"
+                    + str(self.blocos[i][j].j),
                     bd=0.5,
                     relief="solid",
                     bg="white",
                 )
-                if self.blocos[j][i].estado == "vazio":
+                if self.blocos[i][j].estado == "vazio":
                     bloco.config(
                         bg="white",
                         fg="black",
                     )
 
-                if self.blocos[j][i].estado == "inicio":
+                if self.blocos[i][j].estado == "inicio":
                     bloco.config(bg="blue", fg="black")
 
-                if self.blocos[j][i].estado == "fim":
+                if self.blocos[i][j].estado == "fim":
                     bloco.config(bg="red", fg="black")
 
-                if self.blocos[j][i].estado == "caminho":
+                if self.blocos[i][j].estado == "caminho":
                     bloco.config(bg="green", fg="black")
 
-                if self.blocos[j][i].estado == "marcado":
+                if self.blocos[i][j].estado == "marcado":
                     bloco.config(bg="gray", fg="red")
 
-                if self.blocos[j][i].estado == "parede":
+                if self.blocos[i][j].estado == "parede":
                     bloco.config(bg="black", fg="white")
 
-                bloco.grid(row=j, column=i)
-                campo[j][i] = bloco
+                bloco.grid(row=i, column=j)
+                campo[i][j] = bloco
 
     def waveFront(self):
         wave_number = 0
 
         while (
-            self.blocos[self.inicio.j][self.inicio.i].valor == 0
-            and wave_number < max([self.linhas, self.colunas]) + 1
+            self.blocos[self.inicio.i][self.inicio.j].valor == 0
+            and wave_number < self.linhas * self.colunas
         ):
-            for j in range(self.colunas):
-                for i in range(self.linhas):
+            for i in range(self.colunas):
+                for j in range(self.linhas):
                     if (
-                        self.blocos[j][i].estado == "marcado"
-                        or self.blocos[j][i].estado == "fim"
-                    ) and self.blocos[j][i].valor == wave_number:
-                        vizinhos = [(j - 1, i), (j + 1, i), (j, i - 1), (j, i + 1)]
+                        self.blocos[i][j].estado == "marcado"
+                        or self.blocos[i][j].estado == "fim"
+                    ) and self.blocos[i][j].valor == wave_number:
+                        vizinhos = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
 
                         for jAux, iAux in vizinhos:
                             if (
@@ -145,13 +166,43 @@ class Campo:
         blocoAtual = copy.copy(self.inicio)
         passos = 0
 
-        while not (blocoAtual.i == self.fim.i and blocoAtual.j == self.fim.j):
-            vizinhos = [
-                (blocoAtual.j - 1, blocoAtual.i),
-                (blocoAtual.j + 1, blocoAtual.i),
-                (blocoAtual.j, blocoAtual.i - 1),
-                (blocoAtual.j, blocoAtual.i + 1),
-            ]
+        while not (blocoAtual.j == self.fim.j and blocoAtual.i == self.fim.i):
+            print(
+                "Atual: I:",
+                blocoAtual.i,
+                "J:",
+                blocoAtual.j,
+                "Direcao:",
+                blocoAtual.dh + ":" + blocoAtual.dv,
+                "Valor:",
+                blocoAtual.valor,
+                "Estado:",
+                blocoAtual.estado,
+            )
+
+            print()
+            vizinhos = []
+
+            if blocoAtual.dh == "c":
+                vizinhos.append((blocoAtual.i - 1, blocoAtual.j))
+
+            if blocoAtual.dh == "b":
+                vizinhos.append((blocoAtual.i + 1, blocoAtual.j))
+
+            if blocoAtual.dv == "e":
+                vizinhos.append((blocoAtual.i, blocoAtual.j - 1))
+
+            if blocoAtual.dv == "d":
+                vizinhos.append((blocoAtual.i, blocoAtual.j + 1))
+
+            for i, j in vizinhos:
+                print(
+                    "Vizinhos: I:",
+                    i,
+                    "J:",
+                    j,
+                )
+            print()
 
             opcao = []
 
@@ -159,17 +210,129 @@ class Campo:
                 if (
                     0 <= jAux < self.colunas
                     and 0 <= iAux < self.linhas
-                    and self.blocos[jAux][iAux].estado == "marcado"
-                    and self.blocos[jAux][iAux].valor < blocoAtual.valor
+                    and (
+                        self.blocos[jAux][iAux].estado == "marcado"
+                        or self.blocos[jAux][iAux].estado == "vazio"
+                        or self.blocos[jAux][iAux].estado == "fim"
+                    )
+                    and (
+                        self.blocos[jAux][iAux].valor < blocoAtual.valor
+                        or self.blocos[jAux][iAux].valor == 0
+                        or blocoAtual.valor == 0
+                    )
                 ):
+                    print(
+                        "Bloco: I:",
+                        self.blocos[jAux][iAux].i,
+                        "J:",
+                        self.blocos[jAux][iAux].j,
+                        "Direcao:",
+                        self.blocos[jAux][iAux].dh + ":" + self.blocos[jAux][iAux].dv,
+                        "Valor:",
+                        self.blocos[jAux][iAux].valor,
+                        "Estado:",
+                        self.blocos[jAux][iAux].estado,
+                    )
                     opcao.append(self.blocos[jAux][iAux])
 
-            if len(opcao) != 0:
-                escolha = random.choice(opcao)
-            else:
-                return
+            for o in opcao:
+                print(
+                    "Opcao: I:",
+                    o.i,
+                    "J:",
+                    o.j,
+                    "Valor:",
+                    o.valor,
+                    "Estado:",
+                    o.estado,
+                    "Distacia:",
+                    math.sqrt((self.fim.i - o.i) ** 2 + (self.fim.j - o.j) ** 2),
+                )
+            print()
 
-            self.blocos[escolha.j][escolha.i].estado = "caminho"
+            if len(opcao) != 0:
+                escolha = min(
+                    opcao,
+                    key=lambda obj: math.sqrt(
+                        (self.fim.i - obj.i) ** 2 + (self.fim.j - obj.j) ** 2
+                    ),
+                )
+                print(
+                    "Escolha: I:",
+                    escolha.i,
+                    "J:",
+                    escolha.j,
+                    "Valor:",
+                    escolha.valor,
+                    "Estado:",
+                    escolha.estado,
+                )
+                print()
+
+            else:
+                for jAux, iAux in vizinhos:
+                    if (
+                        0 <= jAux < self.colunas
+                        and 0 <= iAux < self.linhas
+                        and (
+                            self.blocos[jAux][iAux].estado == "marcado"
+                            or self.blocos[jAux][iAux].estado == "vazio"
+                        )
+                    ):
+                        print(
+                            "Bloco: I:",
+                            self.blocos[jAux][iAux].i,
+                            "J:",
+                            self.blocos[jAux][iAux].j,
+                            "Direcao:",
+                            self.blocos[jAux][iAux].dh
+                            + ":"
+                            + self.blocos[jAux][iAux].dv,
+                            "Valor:",
+                            self.blocos[jAux][iAux].valor,
+                            "Estado:",
+                            self.blocos[jAux][iAux].estado,
+                        )
+                        opcao.append(self.blocos[jAux][iAux])
+
+                for o in opcao:
+                    print(
+                        "Opcao: I:",
+                        o.i,
+                        "J:",
+                        o.j,
+                        "Valor:",
+                        o.valor,
+                        "Estado:",
+                        o.estado,
+                        "Distacia:",
+                        math.sqrt((self.fim.i - o.i) ** 2 + (self.fim.j - o.j) ** 2),
+                    )
+                print()
+
+                if len(opcao) != 0:
+                    escolha = min(
+                        opcao,
+                        key=lambda obj: math.sqrt(
+                            (self.fim.i - obj.i) ** 2 + (self.fim.j - obj.j) ** 2
+                        ),
+                    )
+                    print(
+                        "Escolha: I:",
+                        escolha.i,
+                        "J:",
+                        escolha.j,
+                        "Valor:",
+                        escolha.valor,
+                        "Estado:",
+                        escolha.estado,
+                    )
+                    print()
+                else:
+                    return
+
+            if self.blocos[escolha.i][escolha.j].estado != "fim":
+                self.blocos[escolha.i][escolha.j].estado = "caminho"
             blocoAtual = copy.copy(escolha)
 
             passos += 1
@@ -177,17 +340,18 @@ class Campo:
 
 root = tk.Tk()
 root.title("Path Finder")
-root.state("zoomed")
+# root.state("zoomed")
 frame = Frame(root)
 frame.grid()
 
 
-campo = Campo(frame, 20, 20)
+campo = Campo(frame, 10, 10)
 
 campo.gerarInicio()
 campo.gerarFim()
 # campo.gerarObstaculos()
 campo.waveFront()
+campo.criarInterface()
 campo.gerarCaminho()
 campo.criarInterface()
 
